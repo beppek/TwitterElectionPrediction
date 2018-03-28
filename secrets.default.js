49ac497e07cf2696f95dfccfaa48b17212510825
beppek <beppe@live.se>
2018-03-28 17:00:16 +0200

    adds secret.default

---
+++ secrets.default.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumerKey: 'consumer key goes here',
+  consumerSecret: 'consumer secret goes here',
+  accessToken: 'access token goes here',
+  accessTokenSecret: 'access token secret goes here'
+};
